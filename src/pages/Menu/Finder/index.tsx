17ce8ef7243f1e0eec526b11acbebc5bea6a8dcc
Emilio Scodeler <scodeler.emilio@gmail.com>
2022-09-02 16:07:53 -0300

    add react icons

---
+++ src/pages/Menu/Finder/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styles from "./Finder.module.scss";
+import { CgSearch } from "react-icons/cg";
+
+interface Props {
+  finder: string;
+  setFinder: React.Dispatch<React.SetStateAction<string>>;
+}
+
+export default function Finder({ finder, setFinder }: Props) {
+  return (
+    <div className={styles.finder}>
+      <input
+        value={finder}
+        onChange={(event) => setFinder(event.target.value)}
+      />
+      <CgSearch size={16} color="#352d29" />
+    </div>
+  );
+}
--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -1,16 +1,23 @@
 import styles from "./Menu.module.scss";
 import { ReactComponent as Logo } from "assets/logo.svg";
+import { useState } from "react";
+import Finder from "./Finder";
 
 export default function Menu() {
+  const [finder, setFinder] = useState("");
   return (
     <main>
-      <nav className={styles.menu}>
-        <Logo />
+      <nav className={styles.navmenu}>
+        <Logo className={styles.logo} />
         <h1>Food House</h1>
       </nav>
       <header className={styles.header}>
         <div className={styles.header__text}>Real food for life</div>
       </header>
+      <section className={styles.menu}>
+        <h3 className={styles.menu__title}>Menu</h3>
+        <Finder finder={finder} setFinder={setFinder} />
+      </section>
     </main>
   );
 }
