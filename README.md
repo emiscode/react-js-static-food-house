3444dcce604534ef2e5f338b6f90f29896728a95
Emilio Scodeler <scodeler.emilio@gmail.com>
2022-09-07 19:24:13 -0300

    final project

--- README.md
+++ README.md
@@ -15,8 +15,11 @@ In the project directory, you can run:
 Runs the app in the development mode.\
 Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
 
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
+<img src="/public/example/main-screenshot.png" alt="main" title="main">
+
+<img src="/public/example/filter-screenshot.png" alt="filter" title="filter">
+
+<img src="/public/example/sort-screenshot.png" alt="sort" title="sort">
 
 ### `npm run build`
 
--- src/pages/Menu/Items/index.tsx
+++ src/pages/Menu/Items/index.tsx
@@ -13,24 +13,43 @@ interface Props {
 export default function Items({ finder, filter, sorter }: Props) {
   const [list, setList] = useState(items);
 
-  const onFinder = (title: string) => {
-    const regex = new RegExp(finder, "i");
-    return regex.test(title);
-  };
-
-  const onFilter = (categoryId: number) => {
-    if (filter) return filter === categoryId;
-
-    return true;
-  };
-
   useEffect(() => {
+    const onFinder = (title: string) => {
+      const regex = new RegExp(finder, "i");
+      return regex.test(title);
+    };
+
+    const onFilter = (categoryId: number) => {
+      if (filter) return filter === categoryId;
+
+      return true;
+    };
+
+    const onSort = (newList: typeof items) => {
+      switch (sorter) {
+        case "portion":
+          return newList.sort((a, b) =>
+            Number(a.size) > Number(b.size) ? 1 : -1
+          );
+        case "how_many_people":
+          return newList.sort((a, b) =>
+            Number(a.serving) > Number(b.serving) ? 1 : -1
+          );
+        case "price":
+          return newList.sort((a, b) =>
+            Number(a.price) > Number(b.price) ? 1 : -1
+          );
+        default:
+          return newList;
+      }
+    };
+
     const newList = items.filter(
       (item) => onFinder(item.title) && onFilter(Number(item.category.id))
     );
 
-    setList(newList);
-  }, [finder, filter]);
+    setList(onSort(newList));
+  }, [finder, filter, sorter]);
 
   return (
     <div className={styles.items}>
